d09160e3fbd2bc59858045d9886ffa0f4f14a54a
VAIBHAV GUPTA <49713974+Vbgpta@users.noreply.github.com>
2020-07-01 21:07:00 +0530

    initial commit

---
+++ AddTwoNumbers.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package addtwonumbers;
+
+/**
+ *
+ * @author User
+ */
+public class AddTwoNumbers {
+
+  public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
+
+    
